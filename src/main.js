bb2425ff03f03c9f66d6c964d78e35e4d3119fed
vichiti <vichiti369@gmail.com>
2025-03-20 17:06:43 +0000

    new way to start vite

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
 
-const app = createApp(App)
-app.use(vuetify)
-app.mount('#app')
+createApp(App).mount('#app')
--- src/plugins/vuetify.js
+++
@@ -1,7 +0,0 @@
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-
-const vuetify = createVuetify()
-
-export default vuetify
