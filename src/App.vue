42f0bd6feb78ebc9ffd11d63c3dc4bb216f758a7
vichiti <vichiti369@gmail.com>
2025-03-20 07:53:43 +0000

    first project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row justify="center" align="center">
+          <h1>Hello</h1>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+</script>
